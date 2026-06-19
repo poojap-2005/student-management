63e353e572307e441707eb7b422c7a44b04aeb6b
pooja patil <pujaptils2912@gmail.com>
2026-06-19 13:42:11 +0530

    Initial React Vite Project

---
+++ student-management/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Dashboard from "./components/Navbar";
+import Navbar from "./pages/Dashboard";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Dashboard />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ student-management/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>🎓 Student Management System</h2>
+    </nav>
+  );
+}
